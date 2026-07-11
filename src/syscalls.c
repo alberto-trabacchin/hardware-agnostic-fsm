f66c6745319aae97e7dd0e58122e215cdc481237
Alberto Trabacchin <alberto.trabacchin@gmail.com>
2026-07-11 17:49:41 +0200

    Removed warnings for syscalls

--- src/syscalls.c
+++ src/syscalls.c
@@ -1,9 +1,39 @@
 #include <sys/stat.h>
+#include <unistd.h>
+#include <errno.h>
 
-// Questi stub evitano i warning di "non implementato"
-int _close(int file) { return -1; }
-int _fstat(int file, struct stat *st) { st->st_mode = S_IFCHR; return 0; }
-int _isatty(int file) { return 1; }
-int _lseek(int file, int ptr, int dir) { return 0; }
-int _read(int file, char *ptr, int len) { return 0; }
-int _write(int file, char *ptr, int len) { return len; }
+int _close(int file) { 
+    (void)file; 
+    return -1; 
+}
+
+int _fstat(int file, struct stat *st) { 
+    (void)file; 
+    st->st_mode = S_IFCHR; 
+    return 0; 
+}
+
+int _isatty(int file) { 
+    (void)file; 
+    return 1; 
+}
+
+int _lseek(int file, int ptr, int dir) { 
+    (void)file; 
+    (void)ptr; 
+    (void)dir; 
+    return 0; 
+}
+
+int _read(int file, char *ptr, int len) { 
+    (void)file; 
+    (void)ptr; 
+    (void)len; 
+    return 0; 
+}
+
+int _write(int file, char *ptr, int len) { 
+    (void)file; 
+    (void)ptr; 
+    return len; // Restituiamo len per far finta che tutto sia stato scritto
+}
